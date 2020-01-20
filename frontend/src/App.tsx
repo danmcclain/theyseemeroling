c30bfa81cac67c16d44ba9bd24858936fba1c79d
Dan McClain <dan@danmcclain.net>
2020-01-19 22:06:53 -0500

    Secondary job, React skeleton

---
+++ frontend/src/App.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Home } from "./pages/home"
+import { About } from "./pages/about"
+import { Dashboard } from "./pages/dashboard"
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <a href="https://discordapp.com/oauth2/authorize?client_id=663033211907211265&scope=bot&permissions=268437504">Add the bot do your discord!</a>
+          </li>
+
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
--- internal/lib/discorder.go
+++ internal/lib/discorder.go
@@ -7,8 +7,18 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+type Guild struct {
+	guild *discordgo.UserGuild
+	roles []*Role
+}
+
+type Role struct {
+	role *discordgo.Role
+}
+
 type Discorder struct {
 	session *discordgo.Session
+	guilds  []Guild
 }
 
 func NewDiscorder(token string) (*Discorder, error) {
@@ -23,6 +33,7 @@ func NewDiscorder(token string) (*Discorder, error) {
 	}
 
 	session.AddHandler(d.ready)
+	session.AddHandler(d.messageCreate)
 
 	err = session.Open()
 
@@ -34,7 +45,24 @@ func NewDiscorder(token string) (*Discorder, error) {
 }
 
 func (d *Discorder) ready(s *discordgo.Session, event *discordgo.Ready) {
-	log.Print("ready speghetti")
+	log.Println("ready speghetti")
+
+	guilds, _ := s.UserGuilds(100, "", "")
+
+	for _, guild := range guilds {
+		// g, _ := s.Guild(guild.ID)
+		log.Printf("Guild: %#v\n", guild)
+		roles, _ := s.GuildRoles(guild.ID)
+
+		for _, role := range roles {
+			log.Printf("Role: %#v\n", role)
+
+		}
+	}
+}
+
+func (d *Discorder) messageCreate(s *discordgo.Session, event *discordgo.MessageCreate) {
+	log.Printf("Message create: %#v\n", event.Message)
 }
 
 func (d *Discorder) Close() {
